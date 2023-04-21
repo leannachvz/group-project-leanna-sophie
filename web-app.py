5419c590ef21ff51193b8bdc8dc24797f5646a83
Leanna Chavez <102705185+leannachvz@users.noreply.github.com>
2023-04-21 12:16:22 -0500

    Login working with DB

--- web-app.py
+++ web-app.py
@@ -1,5 +1,6 @@
 import requests
 import flask_login
+import string
 from os import getenv
 from dotenv import load_dotenv, find_dotenv
 import json
@@ -21,21 +22,21 @@ login_manager.init_app(app)
 users = {'doggy@yay.com': {'password': 'secret'}}
 
 class User(flask_login.UserMixin):
-   pass
+   userCode = 444
 
 @login_manager.user_loader
-def user_loader(email):
-    if email not in users:
+def user_loader(username):
+    if not user_exists(username):
         return
 
     user = User()
-    user.id = email
+    user.id = username
     return user
 
 @login_manager.request_loader
 def request_loader(request):
-    email = request.form.get('email')
-    if email not in users:
+    email = request.form.get('user_id')
+    if not user_exists(email):
         return
 
     user = User()
@@ -43,14 +44,12 @@ def request_loader(request):
     return user
 
 class UserDB(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    movieID = db.Column(db.String(80), unique= False, nullable=True)
-    username = db.Column(db.String(80), unique=False, nullable=False)
-    comment = db.Column(db.String(150), unique=False, nullable=True)
-    rating = db.Column(db.String(3), unique=False, nullable=True)
+    username = db.Column(db.String(80), unique=True, primary_key=True, nullable=False)
+    password = db.Column(db.String(10), unique=False, nullable=False)
+    userCode = db.Column(db.String(10), unique=True)
 
     def __repr__(self) -> str:
-        return f"Person with username: {self.username} and email: {self.comment} and blood: {self.rating}"
+        return f"Person with username: {self.username} "
 
 with app.app_context():
     db.create_all()
@@ -59,20 +58,21 @@ with app.app_context():
 def index():
     return flask.render_template("welcome.html")
 
-def create_user(username):
-   users.append(username)
-   person = UserDB(username=username)
+def create_user(username, password):
+   characters = string.ascii_letters + string.digits
+   while True:
+        userCode = ''.join(random.choices(characters, k=4))
+        if not code_exists(userCode):
+            break
+   person = UserDB(username=username, password=password, userCode=userCode)
    db.session.add(person)
    db.session.commit()
-   
-def create_feedback(movieID, user, newComment, rating):
-   new_feedback = UserDB(movieID=movieID, username=user, comment=newComment, rating=rating)
-   db.session.add(new_feedback)
-   db.session.commit()
-   people = UserDB.query.all()
-   return repr(people)
-   
 
+   flask_login.current_user.userCode = userCode
+   db.session.commit()
+
+   
+   
 @app.route("/login", methods=["POST", "GET"])
 def login():
       return flask.render_template("login.html")
@@ -82,4 +82,56 @@ def login():
 def sign_up():
    return flask.render_template("signup.html")
 
-app.run(DEBUG=True)
+@app.route("/login_verification", methods=["POST"])   
+def login_verification():
+   username = flask.request.form["user_id"]
+   userCheck = UserDB.query.filter_by(username=username).first()
+   if user_exists(username) and flask.request.form["password"] == userCheck.password:
+      user = User()
+      user.id = username
+      flask_login.login_user(user)
+      #flask_login.login_remembered()
+      return flask.redirect(flask.url_for("code_creator"))
+   else:
+      flask.flash("Invalid user credential. Try again!")
+      return flask.redirect(flask.url_for("login"))
+
+@app.route("/signup_check", methods=["POST"])
+def sign_up_check():
+   sign_up_username = flask.request.form["user_id"]
+   sign_up_password = flask.request.form["password"]
+   if user_exists(sign_up_username):# in users:
+      flask.flash("That username already exists, enter a different username!")
+      return flask.redirect(flask.url_for("sign_up"))
+   else:
+      create_user(sign_up_username, sign_up_password)
+      flask.flash("Sign up was successful! Enter your username to login!")
+      return flask.redirect(flask.url_for("login"))
+   
+@app.route("/code_page")
+def code_creator():
+   username = flask_login.current_user.id
+   user = UserDB.query.filter_by(username=username).first()
+   code = user.userCode
+   return f"unique code is {code}"
+
+def user_exists(username):
+   user = UserDB.query.filter_by(username=username).first()
+   if user and user.username == username:
+      return True
+   else:
+      return False
+
+def code_exists(userCode):
+    code = UserDB.query.filter_by(userCode=userCode).first()
+    if code and code.userCode == userCode:
+        return True
+    else:
+        return False
+    
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+app.run()
